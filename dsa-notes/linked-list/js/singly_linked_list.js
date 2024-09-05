40fb281571fd3bc190d21111b77fde362d5a3a34
Gautam Raj <gautamraj996@gmail.com>
2024-09-05 07:42:30 +0530

    vault backup: 2024-09-05 07:42:30

--- dsa-notes/linked-list/js/singly_linked_list.js
+++ dsa-notes/linked-list/js/singly_linked_list.js
@@ -11,3 +11,12 @@ class LinkedList {
     this.size = 0;
   }
 }
+
+// add an element at the end of the list
+// insert element at a position index
+// remove an element from the specified location
+// remove a given element from the list
+// finds the index of element
+// checks the list for empty
+// gives the size of the list
+// prints the list items
