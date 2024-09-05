6c5b56fbc9766411ca8e9f27ec27da577597d055
Gautam Raj <gautamraj996@gmail.com>
2024-09-05 08:11:40 +0530

    vault backup: 2024-09-05 08:11:40

--- dsa-notes/linked-list/js/singly_linked_list.js
+++ dsa-notes/linked-list/js/singly_linked_list.js
@@ -1,28 +1,50 @@
 class Node {
   constructor(data, next = null) {
-    this.data = data;
-    this.next = next;
+    this.data = data; // Initialize data with the provided value
+    this.next = next; // Initialize next with the provided reference, default is null
   }
 }
+
 class LinkedList {
   constructor() {
-    this.head = null;
+    this.head = null; // Initialize the head of the list as null
   }
+
+  // Function to add a new node at the end of the list
   add(data) {
     const newNode = new Node(data);
+    if (!this.head) {
+      // If the list is empty, make the new node the head
+      this.head = newNode;
+    } else {
+      // Otherwise, find the last node and add the new node
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+
+  // Function to print the linked list
+  printList() {
+    let current = this.head;
+    let result = "";
+    while (current) {
+      result += current.data + " -> ";
+      current = current.next;
+    }
+    result += "null";
+    console.log(result);
   }
 }
 
-const L1 = new LinkedList();
+// Creating a new linked list
+const L = new LinkedList();
 const arr = [2, 5, 8, 7];
 
-arr.forEach((num) => L1.add(num));
+// Adding all elements of the array to the linked list
+arr.forEach((num) => L.add(num));
 
-// add an element at the end of the list
-// insert element at a position index
-// remove an element from the specified location
-// remove a given element from the list
-// finds the index of element
-// checks the list for empty
-// gives the size of the list
-// prints the list items
+// Printing the linked list
+L.printList();
